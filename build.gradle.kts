e87f777bbd4db457bdfba143c248d4e65ce6a4f9
sophie <sofija@duck.com>
2024-10-27 12:43:06 +0200

    repo change

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,8 +86,9 @@ publishing {
 
   repositories {
     maven {
-      name = "sadRepo"
-      url = uri("https://repo.sad.ovh/releases")
+      name = "reconnectedRepo"
+      url = uri("https://maven.reconnected.cc/releases")
+
 
       if (!System.getenv("MAVEN_USERNAME").isNullOrEmpty()) {
         credentials {
